1ebf0e4d09519b9b10b12396de763cd999d4d8e6
ismayil095 <isimma095@gmail.com>
2022-04-23 15:36:37 +0400

    Init

---
+++ sql isciler task.sql
@@ -0,0 +1,14 @@
+Create database Hospital
+use Hospital
+create table Isciler
+(Name nvarchar(50),
+Surname nvarchar(100),
+Maas int )
+insert into isciler values
+('Murad','Nerimanli',200),
+('Ceyran','Sultanova',230),
+('Zakir','Ehmedov',280),
+('Mirze','Muslumov',240),
+('Gumrah','Mwrselov',210),
+('Ismayil','Rustemov',180),
+('Samid','Quliyev',190)
